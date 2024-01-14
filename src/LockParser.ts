7803ac9651e7263da7fb454e66d942ff8fe8016f
faissaloux <fwahabali@gmail.com>
2024-01-14 01:25:55 +0100

    parse dependencies in lock file

--- src/LockParser.ts
+++ src/LockParser.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as util from 'util';
 
 export default class LockParser {
     parse(file: string): void {
@@ -7,37 +8,56 @@ export default class LockParser {
         }
 
         const fileContent = fs.readFileSync(file, { encoding: 'utf8', flag: 'r' });
+        const lines = fileContent.split(/\r?\n/);
 
         let object: any = {};
         let parent: string = "";
-        let child: string = "";
+        let section: string = "";
+        let sectionParent: string = "";
+        let lastIndentation: number = 0;
 
-        fileContent.split(/\r?\n/).forEach((line: string) => {
+        lines.forEach((line: string, index: number) => {
             if (!line.startsWith(" ")) {
                 parent = line;
                 object[parent] = [];
-                child = "";
+                section = "";
                 return;
             }
 
             if (line.endsWith(":")) {
-                child = line.replace(":", "").trim();
-                object[parent][child] = [];
-                object[parent][child]["indentation"] = /[a-z]/i.exec(line)?.index;
-                return;
-            } else if (line.includes(":")) {
-                [child, line] = line.split(/:(.*)/s);
-                child = child.trim();
-                object[parent][child] = line.trim();
+                section = line.replace(":", "").trim();
+                object[parent][section] = [];
+                object[parent][section]["indentation"] = /[a-z]/i.exec(line)?.index;
                 return;
             }
 
-            if (child != "") {
-                let lineWithoutParentIndentation = line.slice(object[parent][child]["indentation"]);
-                object[parent][child].push(lineWithoutParentIndentation);
+            if (line.includes(":")) {
+                [section, line] = line.split(/:(.*)/s);
+                section = section.trim();
+                object[parent][section] = line.trim();
+                return;
+            }
+
+            if (section != "") {
+                let lineWithoutParentIndentation: string = line.slice(object[parent][section]["indentation"]);
+                let firstCharIndex: number = /[a-z]/i.exec(lineWithoutParentIndentation)?.index || 0;
+
+                if (lastIndentation == 0 || firstCharIndex <= lastIndentation) {
+                    if (object[parent][section][sectionParent] && firstCharIndex > object[parent][section][sectionParent]["indentation"]) {
+                        object[parent][section][sectionParent].push(line.trim());
+                    } else {
+                        sectionParent = lineWithoutParentIndentation.slice(firstCharIndex);
+                        object[parent][section][sectionParent] = [];
+                        object[parent][section][sectionParent]["indentation"] = firstCharIndex;
+                    }
+                } else if (firstCharIndex > lastIndentation || object[parent][section][sectionParent].length > 0) {
+                    object[parent][section][sectionParent].push(line.trim());
+                }
+
+                lastIndentation = firstCharIndex;
             }
         });
 
-        console.log(object);
+        console.log(util.inspect(object, true, 10));
     }
 }
