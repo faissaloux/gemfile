8467d714c5aef29dfd348e3798389a9a363772e4
faissaloux <fwahabali@gmail.com>
2024-03-24 18:58:56 +0000

    refactor parser tests

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -1,94 +1,80 @@
 import Parser from '../src/Parser';
 
-test('parser one line text contains dependency name', () => {
+describe('text parser', () => {
   const parser = new Parser();
 
-  let parsed = parser.text(`
-    gem "error_highlight"
-  `).parse();
+  test('parse one line contains dependency name', () => {
+    let parsed = parser.text(`
+      gem "error_highlight"
+    `).parse();
 
-  expect(parsed).toBe(`{"dependencies":[{"name":"error_highlight"}]}`);
-});
+    expect(parsed).toBe(`{"dependencies":[{"name":"error_highlight"}]}`);
+  });
 
-test('parser one line text contains dependency name and simple version', () => {
-  const parser = new Parser();
+  test('parse one line contains dependency name and simple version', () => {
+    let parsed = parser.text(`
+      gem "error_highlight", ">= 0.4.0"
+    `).parse();
 
-  let parsed = parser.text(`
-    gem "error_highlight", ">= 0.4.0"
-  `).parse();
+    expect(parsed).toBe(`{"dependencies":[{"name":"error_highlight","version":">= 0.4.0"}]}`);
+  });
 
-  expect(parsed).toBe(`{"dependencies":[{"name":"error_highlight","version":">= 0.4.0"}]}`);
-});
+  test('parse one line contains dependency name and composed version', () => {
+    let parsed = parser.text(`
+      gem "json", ">= 2.0.0", "!=2.7.0"
+    `).parse();
 
-test('parser one line text contains dependency name and composed version', () => {
-  const parser = new Parser();
+    expect(parsed).toBe(`{"dependencies":[{"name":"json","version":">= 2.0.0, != 2.7.0"}]}`);
+  });
 
-  let parsed = parser.text(`
-    gem "json", ">= 2.0.0", "!=2.7.0"
-  `).parse();
-
-  expect(parsed).toBe(`{"dependencies":[{"name":"json","version":">= 2.0.0, != 2.7.0"}]}`);
-});
-
-test('parser one line text contains dependency name, version and one platform', () => {
-  const parser = new Parser();
-
-  let parsed = parser.text(`
-    gem "error_highlight", ">= 0.4.0", platforms: :ruby
-  `).parse();
-
-  expect(parsed).toBe(`{"dependencies":[{"name":"error_highlight","version":">= 0.4.0","platforms":["ruby"]}]}`);
-});
-
-test('parser one line text contains dependency name, version and multiple platforms', () => {
-  const parser = new Parser();
-
-  let parsed = parser.text(`
-    gem "json", ">= 2.0.0", "!=2.7.0", platforms: [:windows, :jruby]
-  `).parse();
-
-  expect(parsed).toBe(`{"dependencies":[{"name":"json","version":">= 2.0.0, != 2.7.0","platforms":["windows","jruby"]}]}`);
-});
-
-test('parser one line text contains dependency name and git', () => {
-  const parser = new Parser();
-
-  let parsed = parser.text(`
-    gem "sdoc", git: "https://github.com/rails/sdoc.git"
-  `).parse();
-
-  expect(parsed).toBe(`{"dependencies":[{"name":"sdoc","git":"https://github.com/rails/sdoc.git"}]}`);
-});
-
-test('parser one line text contains dependency name, git and branch', () => {
-  const parser = new Parser();
-
-  let parsed = parser.text(`
-    gem "sdoc", git: "https://github.com/rails/sdoc.git", branch: "main"
-  `).parse();
-
-  expect(parsed).toBe(`{"dependencies":[{"name":"sdoc","git":"https://github.com/rails/sdoc.git","branch":"main"}]}`);
-});
-
-test('parser one line text contains dependency name and require', () => {
-  const parser = new Parser();
-
-  let parsed = parser.text(`
-    gem "sdoc", require: false
-  `).parse();
-
-  expect(parsed).toBe(`{"dependencies":[{"name":"sdoc","require":"false"}]}`);
-});
-
-test('parser multiple lines text', () => {
-  const parser = new Parser();
-
-  let parsed = parser.text(`
-      gem "json", ">= 2.0.0", "!=2.7.0", platforms: [:windows, :jruby]
+  test('parse one line contains dependency name, version and one platform', () => {
+    let parsed = parser.text(`
       gem "error_highlight", ">= 0.4.0", platforms: :ruby
-      gem "sdoc", git: "https://github.com/rails/sdoc.git", branch: "main"
-      gem "websocket-client-simple", github: "matthewd/websocket-client-simple", branch: "close-race", require: false
-  `).parse();
+    `).parse();
 
-  expect(parsed).toBe(`{"dependencies":[{"name":"json","version":">= 2.0.0, != 2.7.0","platforms":["windows","jruby"]},{"name":"error_highlight","version":">= 0.4.0","platforms":["ruby"]},{"name":"sdoc","git":"https://github.com/rails/sdoc.git","branch":"main"},{"name":"websocket-client-simple","require":"false","github":"matthewd/websocket-client-simple","branch":"close-race"}]}`);
+    expect(parsed).toBe(`{"dependencies":[{"name":"error_highlight","version":">= 0.4.0","platforms":["ruby"]}]}`);
+  });
+
+  test('parse one line contains dependency name, version and multiple platforms', () => {
+    let parsed = parser.text(`
+      gem "json", ">= 2.0.0", "!=2.7.0", platforms: [:windows, :jruby]
+    `).parse();
+
+    expect(parsed).toBe(`{"dependencies":[{"name":"json","version":">= 2.0.0, != 2.7.0","platforms":["windows","jruby"]}]}`);
+  });
+
+  test('parse one line contains dependency name and git', () => {
+    let parsed = parser.text(`
+      gem "sdoc", git: "https://github.com/rails/sdoc.git"
+    `).parse();
+
+    expect(parsed).toBe(`{"dependencies":[{"name":"sdoc","git":"https://github.com/rails/sdoc.git"}]}`);
+  });
+
+  test('parse one line contains dependency name, git and branch', () => {
+    let parsed = parser.text(`
+      gem "sdoc", git: "https://github.com/rails/sdoc.git", branch: "main"
+    `).parse();
+
+    expect(parsed).toBe(`{"dependencies":[{"name":"sdoc","git":"https://github.com/rails/sdoc.git","branch":"main"}]}`);
+  });
+
+  test('parse one line contains dependency name and require', () => {
+    let parsed = parser.text(`
+      gem "sdoc", require: false
+    `).parse();
+
+    expect(parsed).toBe(`{"dependencies":[{"name":"sdoc","require":"false"}]}`);
+  });
+
+  test('parse multiple lines', () => {
+    let parsed = parser.text(`
+        gem "json", ">= 2.0.0", "!=2.7.0", platforms: [:windows, :jruby]
+        gem "error_highlight", ">= 0.4.0", platforms: :ruby
+        gem "sdoc", git: "https://github.com/rails/sdoc.git", branch: "main"
+        gem "websocket-client-simple", github: "matthewd/websocket-client-simple", branch: "close-race", require: false
+    `).parse();
+
+    expect(parsed).toBe(`{"dependencies":[{"name":"json","version":">= 2.0.0, != 2.7.0","platforms":["windows","jruby"]},{"name":"error_highlight","version":">= 0.4.0","platforms":["ruby"]},{"name":"sdoc","git":"https://github.com/rails/sdoc.git","branch":"main"},{"name":"websocket-client-simple","require":"false","github":"matthewd/websocket-client-simple","branch":"close-race"}]}`);
+  });
 });
