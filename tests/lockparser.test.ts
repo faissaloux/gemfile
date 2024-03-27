1cb2d0e31291869dd07abd6e8edcd0d42947a673
faissaloux <fwahabali@gmail.com>
2024-03-27 00:57:15 +0000

    all in LockParser test

--- tests/lockparser.test.ts
+++ tests/lockparser.test.ts
@@ -86,7 +86,8 @@ GIT
 
         test('parse multiple sections with same name with whitespace in empty line', () => {
             const parser = new LockParser();
-            let parsed = parser.text(`GIT
+            let parsed = parser.text(`
+GIT
     remote: https://github.com/matthewd/websocket-client-simple.git
     revision: e161305f1a466b9398d86df3b1731b03362da91b
     branch: close-race
@@ -140,6 +141,66 @@ PLATFORMS
 `).parse();
 
             expect(parsed).toBe(`{"PLATFORMS":["ruby","x86_64-darwin","x86_64-linux"]}`);
+        });
+
+        test('parse all in', () => {
+            const parser = new LockParser();
+            let parsed = parser.text(`
+PATH
+    remote: .
+    specs:
+    actioncable (7.2.0.alpha)
+        actionpack (= 7.2.0.alpha)
+        activesupport (= 7.2.0.alpha)
+        nio4r (~> 2.0)
+        websocket-driver (>= 0.6.1)
+        zeitwerk (~> 2.6)
+
+GIT
+    remote: https://github.com/matthewd/websocket-client-simple.git
+    revision: e161305f1a466b9398d86df3b1731b03362da91b
+    branch: close-race
+    specs:
+        websocket-client-simple (0.3.0)
+        event_emitter
+        websocket
+
+GIT
+    remote: https://github.com/rails/sdoc.git
+    revision: 08b4252b1f5d185890562f4106d24f3afa944e40
+    branch: main
+    specs:
+        sdoc (3.0.0.alpha)
+        nokogiri
+        rdoc (>= 5.0)
+        rouge
+
+GEM
+    remote: https://rubygems.org/
+    specs:
+    addressable (2.8.6)
+        public_suffix (>= 2.0.2, < 6.0)
+    amq-protocol (2.3.2)
+    ast (2.4.2)
+    aws-eventstream (1.3.0)
+    aws-partitions (1.876.0)
+    aws-sdk-core (3.190.1)
+        aws-eventstream (~> 1, >= 1.3.0)
+        aws-partitions (~> 1, >= 1.651.0)
+        aws-sigv4 (~> 1.8)
+        jmespath (~> 1, >= 1.6.1)
+
+PLATFORMS
+    ruby
+    x86_64-darwin
+    x86_64-linux
+
+DEPENDENCIES
+    activerecord-jdbcmysql-adapter (>= 1.3.0)
+    aws-sdk-s3
+`).parse();
+        
+            expect(parsed).toBe(`{"PATH":{"remote":".","specs":{"actioncable (7.2.0.alpha)":["actionpack (= 7.2.0.alpha)","activesupport (= 7.2.0.alpha)","nio4r (~> 2.0)","websocket-driver (>= 0.6.1)","zeitwerk (~> 2.6)"]}},"GIT":[{"remote":"https://github.com/matthewd/websocket-client-simple.git","revision":"e161305f1a466b9398d86df3b1731b03362da91b","branch":"close-race","specs":{"websocket-client-simple (0.3.0)":[],"event_emitter":[],"websocket":[]}},{"remote":"https://github.com/rails/sdoc.git","revision":"08b4252b1f5d185890562f4106d24f3afa944e40","branch":"main","specs":{"sdoc (3.0.0.alpha)":[],"nokogiri":[],"rdoc (>= 5.0)":[],"rouge":[]}}],"GEM":{"remote":"https://rubygems.org/","specs":{"addressable (2.8.6)":["public_suffix (>= 2.0.2, < 6.0)"],"amq-protocol (2.3.2)":[],"ast (2.4.2)":[],"aws-eventstream (1.3.0)":[],"aws-partitions (1.876.0)":[],"aws-sdk-core (3.190.1)":["aws-eventstream (~> 1, >= 1.3.0)","aws-partitions (~> 1, >= 1.651.0)","aws-sigv4 (~> 1.8)","jmespath (~> 1, >= 1.6.1)"]}},"PLATFORMS":["ruby","x86_64-darwin","x86_64-linux"],"DEPENDENCIES":["activerecord-jdbcmysql-adapter (>= 1.3.0)","aws-sdk-s3"]}`);
         });
   });
 });
